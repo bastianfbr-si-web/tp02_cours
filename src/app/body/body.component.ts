cbd3bdddbfa729c4bb96fab66638dc4daf9548c1
Bastian <badmaxyt@gmail.com>
2024-02-01 16:56:47 +0100

    feat: :sparkles: add button and form

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
+  nom : string = "Bastian";
+  valid : boolean = true;
+  message : string = "";
+  compteur : number = 0;
 
+  click() {
+    console.log("click");
+    this.compteur++;
+    this.message = ("Vous avez cliqué " + this.compteur + " fois !");
+    if (this.compteur > 10) {
+      this.valid = false;
+      this.message = "Vous avez cliqué trop de fois !";
+    }
+  }
 }
